76abe3605ebff1a81ac6666f657c3b4b327265dc
think <2812364443@qq.com>
2020-06-01 00:24:29 +0800

    一个源文件夹导致的 没有及时处理 耽误了睡眠

---
+++ src/main/java/com/baizhi/controller/UnderstandSourceDirectory.java
@@ -0,0 +1,9 @@
+package com.baizhi.controller;
+
+public class UnderstandSourceDirectory {
+
+
+    public static void main(String[] args) {
+        System.out.println("因为不理解源项目文件夹 导致折腾半天 该打该打 实际是idea 如何处理文件夹的一个设置");
+    }
+}
