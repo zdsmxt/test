5d8c2c3815397b59298e79afebffea6bd8cf8cde
think <2812364443@qq.com>
2020-05-31 22:25:05 +0800

    来到github社区

--- src/main/java/com/baizhi/controller/HelloController.java
+++ src/main/java/com/baizhi/controller/HelloController.java
@@ -1,16 +1,10 @@
 package com.baizhi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String helloName(String name){
-        System.out.println("姓名:  "+name);
-        return "hello: "+name;
+    public static void main(String[] args) {
+        System.out.println("你好 外网的github");
     }
 
 }
