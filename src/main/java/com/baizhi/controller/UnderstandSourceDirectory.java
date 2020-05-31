cf4f843b8748677abbbdb86849f119a310ad7796
think <2812364443@qq.com>
2020-06-01 00:27:12 +0800

    理解文件夹 再添注释

--- src/main/java/com/baizhi/controller/UnderstandSourceDirectory.java
+++ src/main/java/com/baizhi/controller/UnderstandSourceDirectory.java
@@ -2,7 +2,7 @@ package com.baizhi.controller;
 
 public class UnderstandSourceDirectory {
 
-
+    //项目名下的 src main 和 recources 存放的文件不同  文件夹的颜色有所不同
     public static void main(String[] args) {
         System.out.println("因为不理解源项目文件夹 导致折腾半天 该打该打 实际是idea 如何处理文件夹的一个设置");
     }
