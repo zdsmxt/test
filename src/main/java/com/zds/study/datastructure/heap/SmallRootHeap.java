d9a053ba427e7321e81fb8e005fd857ab5399d0e
think <2812364443@qq.com>
2020-06-11 23:43:01 +0800

    解决maven 下载lastUppdated 问题

---
+++ src/main/java/com/zds/study/datastructure/heap/SmallRootHeap.java
@@ -0,0 +1,11 @@
+package com.zds.study.datastructure.heap;
+
+import java.util.PriorityQueue;
+
+public class SmallRootHeap {
+    public static void main(String[] args) {
+        //底层实现 小根堆
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+    }
+}
