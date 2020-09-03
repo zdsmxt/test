f2bd8995894c738e39bb7a0f79243fca3dcee98d
zds <2812364443@qq.com>
2020-09-04 00:56:30 +0800

    2020 链表的指针 指向

--- src/main/java/com/zds/study/datastructure/heap/SmallRootHeap.java
+++ src/main/java/com/zds/study/datastructure/heap/SmallRootHeap.java
@@ -4,7 +4,8 @@ import java.util.PriorityQueue;
 
 public class SmallRootHeap {
     public static void main(String[] args) {
-        //底层实现 小根堆
+        //底层实现 小根堆  優先級隊列 涉及到樹的結構 都會有對數函數log(N)
+        //的複雜度 多試試 多试试 就好了 再試試
         PriorityQueue<Integer> queue = new PriorityQueue<>();
 
     }
