99463e2f64933ca6429698f526b82ec2e23a66e7
zds <2812364443@qq.com>
2020-09-27 22:24:04 +0800

    多看多想 多理解 慢慢就会发现规律 得心应手了

--- src/main/java/com/zds/study/thread/ThreadJoin.java
+++ src/main/java/com/zds/study/thread/ThreadJoin.java
@@ -4,12 +4,32 @@ package com.zds.study.thread;
 public class ThreadJoin {
 
     public static void main(String[] args) throws InterruptedException{
-        Thread t1=new Thread(()->{
-            System.out.println("T1============");
-        });
 
-        t1.start();
-        t1.join();
+        Thread t1 =null;
+        Thread t2 =null;
+        Thread t3 =null;
+            t1=new Thread(()->{
+                System.out.println("T1============");
+            });
+            t2=new Thread(()->{
+                System.out.println("T2============");
+            });
+            t3=new Thread(()-> {
+                System.out.println("T3============");
+
+            });
+
+//        for (int i = 0; i < 2; i++) {
+            t1.start();
+            t2.start();
+            t3.start();
+//            t1.join();
+            t2.join();
+            t3.join();
+
+
+//        }
+
         System.out.println("main end");
     }
 
