3e90c0a7915b73362a6a1c0de08bb6cbdbcc60a1
zhudongsheng <zhudongsheng@kungeek.com>
2021-01-11 17:16:14 +0800

    [daylog20210111]:test2 merge to 202101

---
+++ src/main/java/com/zds/study/jdk/tree/Node.java
@@ -0,0 +1,41 @@
+package com.zds.study.jdk.tree;
+
+public class Node {
+	private Integer id;
+	private String name;
+	private String link;
+	private Integer parentId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public Integer getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Integer parentId) {
+		this.parentId = parentId;
+	}
+
+}
